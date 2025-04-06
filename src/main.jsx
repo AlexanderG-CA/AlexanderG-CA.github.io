a2ce8108c742bbcda8587ce75fcd5f1fe5055afb
AlexanderG-CA <alexander.gorie@chasacademy.se>
2025-04-06 22:45:30 +0200

    First commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './assets/styles.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/styles.css'; // Importera CSS
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Importera React Router
+import App from './App.jsx'; // Importera App-komponenten
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* BrowserRouter används för att hantera routning */}
+    <BrowserRouter basename="/AlexanderG-CA.github.io/"> {/* Ange rätt basväg här */}
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* Lägg till fler rutter om du har flera sidor */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
